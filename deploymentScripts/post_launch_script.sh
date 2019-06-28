902f26db357c85132ee85c632849f4de907d2d86
Łukasz Mitka <lukasz.mitka@husarion.com>
2019-06-28 13:16:22 +0200

    Update key file name

--- deploymentScripts/post_launch_script.sh
+++ deploymentScripts/post_launch_script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ssh -i "~/.ssh/outsource_test.pem" -tt ubuntu@ec2-instance /home/ubuntu/ros_workspace/src/tutorial_pkg/scripts/gmapping.sh &
+ssh -i "~/.ssh/rosbot_key.pem" -tt ubuntu@ec2-instance /home/ubuntu/ros_workspace/src/tutorial_pkg/scripts/gmapping.sh &
 exit 0
