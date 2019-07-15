83cc1bda1e6cfb42000481d4ec9af82993d1f2af
Łukasz Mitka <lukasz.mitka@husarion.com>
2019-07-15 16:03:47 +0200

    Move ec2 outsourcing to post launch script

--- deploymentScripts/post_launch_script.sh
+++ deploymentScripts/post_launch_script.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-ssh -i "~/.ssh/rosbot_key.pem" -tt ubuntu@ec2-instance /home/ubuntu/ros_workspace/src/tutorial_pkg/scripts/gmapping.sh &
+echo "Start post launch"
+nohup ssh -i "~/.ssh/rosbot_key.pem"  ubuntu@ec2-instance /home/ubuntu/ros_workspace/src/tutorial_pkg/scripts/gmapping.sh >/dev/null 2>&1 &
+sleep 1
+echo "SSH finished"
 exit 0
--- scripts/gmapping.sh
+++ scripts/gmapping.sh
@@ -5,4 +5,9 @@ export ROS_MASTER_URI=http://master:11311
 
 . /opt/ros/kinetic/setup.sh
 . ~/ros_workspace/devel/setup.sh
-roslaunch tutorial_pkg tutorial_robomaker_1_ec2.launch
+echo "Launching tutorial_robomaker_1_ec2.launch"
+roslaunch tutorial_pkg tutorial_robomaker_1_ec2.launch &
+echo "process started"
+disown
+echo "Process disowned"
+exit 0
