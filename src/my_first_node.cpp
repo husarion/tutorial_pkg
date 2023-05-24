63f69ed3bb0037febc95a3e753e665e4dad345ef
rafal-gorecki <rafal-gorecki@husarion.com>
2023-05-24 20:24:13 +0200

    Add timer & service client

--- src/my_first_node.cpp
+++ src/my_first_node.cpp
@@ -1,31 +1,64 @@
+#include <chrono>
+
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/u_int8.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_srvs/srv/empty.hpp"
 
-void imageCallback(const sensor_msgs::msg::Image::SharedPtr image)
+using namespace std::chrono_literals;
+class MyNode : public rclcpp::Node
 {
-    long long sum = 0;
-    for (uint8_t value : image->data)
-    {
-        sum += value;
-    }
-    int avg = sum / image->data.size();
-    RCLCPP_INFO(rclcpp::get_logger("my_node"), "Brightness: %d", avg);
-}
+public:
+   MyNode() : Node("my_node")
+   {
+      subscriber_ = create_subscription<sensor_msgs::msg::Image>(
+         "/image", 3, std::bind(&MyNode::image_callback, this, std::placeholders::_1));
+      publisher_ = create_publisher<std_msgs::msg::UInt8>("/brightness", 10);
+      timer_ = create_wall_timer(5s, std::bind(&MyNode::timer_callback, this));
+      service_client_ = create_client<std_srvs::srv::Empty>("/save");
+
+      RCLCPP_INFO(get_logger(), "Node started!");
+   }
+
+private:
+   void image_callback(const sensor_msgs::msg::Image::SharedPtr image)
+   {
+      long long sum = 0;
+      for (uint8_t value : image->data)
+      {
+         sum += value;
+      }
+      int avg = sum / image->data.size();
+
+      std_msgs::msg::UInt8 brightness_msg;
+      brightness_msg.data = avg;
+      publisher_->publish(brightness_msg);
+   }
+
+   void timer_callback()
+   {
+      if (!service_client_->wait_for_service(1s))
+      {
+         RCLCPP_ERROR(get_logger(), "Failed to connect to the image save service");
+         return;
+      }
+
+      auto request = std::make_shared<std_srvs::srv::Empty::Request>();
+      auto future = service_client_->async_send_request(request);
+   }
+
+   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscriber_;
+   rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr publisher_;
+   rclcpp::TimerBase::SharedPtr timer_;
+   rclcpp::Client<std_srvs::srv::Empty>::SharedPtr service_client_;
+};
 
 int main(int argc, char **argv)
 {
-   rclcpp::init(argc, argv);
-   auto node = rclcpp::Node::make_shared("my_node");
-   rclcpp::Rate loop_rate(30);  
-
-   auto subscriber = node->create_subscription<sensor_msgs::msg::Image>(
-      "/camera/image", 3, imageCallback);
-
-   RCLCPP_INFO(node->get_logger(), "Node started!");
-   while (rclcpp::ok())
-   {
-      rclcpp::spin_some(node);
-      loop_rate.sleep();
-   }
-   return 0;
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<MyNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
 }
