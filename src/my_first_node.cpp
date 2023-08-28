edb0b8fb28b24b5567de35bc04a789c6f7f35074
rafal-gorecki <rafal.gorecki@husarion.com>
2023-08-28 19:15:02 +0200

    refactor

--- src/my_first_node.cpp
+++ src/my_first_node.cpp
@@ -1,14 +1,13 @@
-
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <std_msgs/UInt8.h>
 #include <std_srvs/Empty.h>
 #include <std_srvs/Trigger.h>
 
-bool print_b;
+uint saved_imgs = 0;
+
 ros::Publisher brightness_pub;
-int frames_passed = 0;
-int saved_imgs = 0;
+ros::ServiceClient client;
 
 void imageCallback(const sensor_msgs::ImageConstPtr &image)
 {
@@ -18,14 +17,19 @@ void imageCallback(const sensor_msgs::ImageConstPtr &image)
       sum += value;
    }
    int avg = sum / image->data.size();
-   if (print_b)
-   {
-      std::cout << "Brightness: " << avg << std::endl;
-   }
+
    std_msgs::UInt8 brightness_value;
    brightness_value.data = avg;
    brightness_pub.publish(brightness_value);
-   frames_passed++;
+}
+
+void timerCallback(const ros::TimerEvent&)
+{
+   ROS_INFO("Timer activate");
+
+   std_srvs::Empty srv;
+   client.call(srv);
+   saved_imgs++;
 }
 
 bool saved_img(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
@@ -39,24 +43,16 @@ int main(int argc, char **argv)
 {
    ros::init(argc, argv, "my_first_node");
    ros::NodeHandle n("~");
+
+   double timer_period_s;
+   n.param("timer_period_s", timer_period_s, 5.0);
+   
    ros::Subscriber sub = n.subscribe("/camera/color/image_raw", 10, imageCallback);
-   n.param<bool>("print_brightness", print_b, false);
    brightness_pub = n.advertise<std_msgs::UInt8>("brightness", 1);
-   ros::ServiceClient client = n.serviceClient<std_srvs::Empty>("/image_saver/save");
-   std_srvs::Empty srv;
-   ros::ServiceServer service = n.advertiseService("saved_images", saved_img);
-   ros::Rate loop_rate(30);
+   ros::Timer timer = n.createTimer(ros::Duration(timer_period_s), timerCallback);
+   client = n.serviceClient<std_srvs::Empty>("/image_saver/save");
+   ros::ServiceServer service = n.advertiseService("image_counter", saved_img);
 
    ROS_INFO("Node started!");
-   while (ros::ok())
-   {
-      ros::spinOnce();
-      if (frames_passed > 100)
-      {
-         client.call(srv);
-         frames_passed = 0;
-         saved_imgs++;
-      }
-      loop_rate.sleep();
-   }
+   ros::spin();
 }
